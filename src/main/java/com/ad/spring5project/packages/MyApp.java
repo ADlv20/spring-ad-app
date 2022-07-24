8d50939a8f5d30aa84e1ad493e9a9a64229ce78f
ADlv19 <adwaitg02@gmail.com>
2022-07-24 22:35:27 +0530

    Init

---
+++ src/main/java/com/ad/spring5project/packages/MyApp.java
@@ -0,0 +1,27 @@
+package com.ad.spring5project.packages;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+public class MyApp {
+
+    @GetMapping("/")
+    public Student helloExample(){
+        return new Student(1,"ADwait","IT");
+    }
+
+    @GetMapping("/list")
+    public List<Student> listOfStudents(){
+        return List.of(
+                new Student(1,"ADwait","IT"),
+                new Student(1,"AD","CS"),
+                new Student(1,"wait","Mech"),
+                new Student(1,"lucky","Extc")
+        );
+    }
+}
---
+++ src/test/java/com/ad/spring5project/Spring5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.spring5project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
